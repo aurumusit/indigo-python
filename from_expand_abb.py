90bf3555d3c45b54c376b1e12d1fac6db564e2cd
aurumusit <107499065+aurumusit@users.noreply.github.com>
2023-10-26 16:50:06 -0500

    Explored till canonical smiles 26/10

--- from_expand_abb.py
+++ from_expand_abb.py
@@ -1,7 +1,7 @@
-# import indigo
+import indigo
 
 # # Create an Indigo instance
-# indigo_instance = indigo.Indigo()
+indigo_instance = indigo.Indigo()
 
 # # Load a molecule from a Molfile with aliases
 # mol = indigo_instance.loadMolecule("OC([*:1])[*:2]")
@@ -42,13 +42,121 @@
 #     cml_file.write(cml)
 
 
+## Calculating Properties
+
+# # Create an Indigo instance
+# indigo_instance = indigo.Indigo()
+
+# # Load a molecule from a Molfile
+# mol = indigo_instance.loadMolecule("CC(=O)C1C(=CC(Br)=CC=1OC)OC")
+
+# # Calculate the number of atoms
+# num_atoms = mol.countAtoms()
+# print(f"Number of atoms: {num_atoms}")
+
+# num_pseudoatoms = mol.countPseudoatoms()
+# print(f"Number of pseudoatoms: {num_pseudoatoms}")
+
+# num_rsites = mol.countRSites()
+# print(f"Number of R-sites: {num_rsites}")
+
+# num_bonds = mol.countBonds()
+# print(f"Number of bonds: {num_bonds}")
+
+
+# formula = mol.grossFormula()
+# print(f"Gross formula: {formula}")
+
+
+# weight = mol.molecularWeight()
+# print(f"Molecular weight: {weight} g/mol")
+
+# abundant_mass = mol.mostAbundantMass()
+# print(f"Most abundant isotopes mass: {abundant_mass} g/mol")
+
+
+# monoisotopic = mol.monoisotopicMass()
+# print(f"Monoisotopic mass: {monoisotopic} g/mol")
+
+
+# has_coordinates = mol.hasCoord()
+# print(f"Has coordinates: {has_coordinates}")
+
+# has_3d_coordinates = mol.hasZCoord()
+# print(f"Has 3D coordinates: {has_3d_coordinates}")
+
+
+# is_chiral = mol.isChiral()
+# print(f"Is chiral: {is_chiral}")
+
+
+# heavy_atoms = mol.countHeavyAtoms()
+# print(f"Number of heavy atoms (excluding hydrogens): {heavy_atoms}")
+
+# implicit_hydrogens = mol.countImplicitHydrogens()
+# print(f"Number of implicit hydrogens: {implicit_hydrogens}")
 
 
 
+# total_hydrogens = mol.countHydrogens()
+# print(f"Total number of hydrogens: {total_hydrogens}")
+
+# sssr_count = mol.countSSSR()
+# print(f"Number of SSSR cycles: {sssr_count}")
 
 
+# Create an Indigo instance
+# indigo_instance = indigo.Indigo()
+
+# # Load a molecule with bad valence
+# mol_with_bad_valence = indigo_instance.loadMolecule("CC(=O)[O]")
+
+# # Check for bad valence
+# valence_issues = mol_with_bad_valence.checkBadValence()
+
+# if valence_issues:
+#     print(f"Molecule has bad valence issues: {valence_issues}")
+# else:
+#     print("Molecule has correct valence.")
 
 
+# # Load a molecule with ambiguous hydrogen positions
+# mol_with_ambiguous_h = indigo_instance.loadMolecule("C(C)(C)(C)(C)(C)")
+
+# # Check for ambiguous hydrogen positions
+# h_position_issues = mol_with_ambiguous_h.checkAmbiguousH()
+
+# if h_position_issues:
+#     print(f"Molecule has ambiguous hydrogen positions: {h_position_issues}")
+# else:
+#     print("Molecule has unambiguous hydrogen positions.")
+
+##connected components
 
 
+# Load a molecule
+mol = indigo_instance.loadMolecule("CCO.CC1=CC=CC=C1.CC(=O)O")
 
+# Count the number of connected components
+component_count = mol.countComponents()
+print(f"{component_count} components")
+
+# Iterate over the components
+for comp in mol.iterateComponents():
+    # Clone the component and print its SMILES representation
+    print(comp.clone().smiles())
+    
+    # Print information about the component
+    print(f"component {comp.index()}: {comp.countAtoms()} atoms, {comp.countBonds()} bonds")
+    
+    # Iterate over atoms in the component and print their indices
+    for atom in comp.iterateAtoms():
+        print(atom.index())
+
+# Iterate over all atoms in the original molecule and print their component indices
+for atom in mol.iterateAtoms():
+    print(f"Atom {atom.index()} belongs to component {atom.componentIndex()}")
+
+# Iterate over atoms in a specific component (component 0 in this example) and print their indices
+for atom in mol.component(0).iterateAtoms():
+    print(f"Atom {atom.index()} in component 0")
